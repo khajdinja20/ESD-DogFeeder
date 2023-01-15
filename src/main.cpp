e9ec55746becef79cd0cc001736d9ffc68cd3988
Teodor Kolar <tkolar20@foi.hr>
2023-01-15 14:19:16 +0100

    AI code to be integrated into main

--- src/main.cpp
+++ src/main.cpp
@@ -1,205 +1,101 @@
 #include <Arduino.h>
 #include <WiFi.h>
-#include <Update.h>
-#include <HTTPClient.h>
+#include <driver/i2s.h>
+#include <esp_task_wdt.h>
+#include "I2SMicSampler.h"
+#include "ADCSampler.h"
+#include "config.h"
+#include "CommandDetector.h"
+#include "CommandProcessor.h"
 
-// pin variables
+// i2s config for using the internal ADC
+i2s_config_t adcI2SConfig = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_ADC_BUILT_IN),
+    .sample_rate = 16000,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,
+    .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
+    .communication_format = I2S_COMM_FORMAT_I2S_LSB,
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 4,
+    .dma_buf_len = 1024,
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0};
 
-const int actionPin = 22;
-const int redPin = 13;
-const int grnPin = 14;
-const int bluPin = 15;
-const int trigPin = 19;
-const int echoPin = 18;
-const int micPinDigital = 23;
-const int micPinAnalogue = 4;
+// i2s config for reading from both channels of I2S
+i2s_config_t i2sMemsConfigBothChannels = {
+    .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
+    .sample_rate = 16000,
+    .bits_per_sample = I2S_BITS_PER_SAMPLE_32BIT,
+    .channel_format = I2S_MIC_CHANNEL,
+    .communication_format = i2s_comm_format_t(I2S_COMM_FORMAT_I2S),
+    .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
+    .dma_buf_count = 4,
+    .dma_buf_len = 1024,
+    .use_apll = false,
+    .tx_desc_auto_clear = false,
+    .fixed_mclk = 0};
 
-// stored variables for wifi setup
+// i2s microphone pins
+i2s_pin_config_t i2s_mic_pins = {
+    .bck_io_num = I2S_MIC_SERIAL_CLOCK,
+    .ws_io_num = I2S_MIC_LEFT_RIGHT_CLOCK,
+    .data_out_num = I2S_PIN_NO_CHANGE,
+    .data_in_num = I2S_MIC_SERIAL_DATA};
 
-const char *SSID = "Hajdinjak";
-const char *SSIDpassword = "7595zec590";
-int timeOutCount = 0;
-
-// variables for update(not yet implemented)
-
-const char *url;
-const char *host;
-const char *file;
-String type = "Smart DogFeeder";
-const float currentVersion = 0.2;
-
-// stored variables for microphone (not yet implemented)
-
-int micValueDigi;
-int micValueAnalogue;
-
-// stored variables for distance senzor
-
-int distanceThreshold = 50;
-long distance;
-long duration;
-
-// variables to negate false positives
-
-bool distanceActive = false;
-bool soundActive = false;
-
-// function that activates distance senzor and returns the distance in cm
-long measureDistance()
+// This task does all the heavy lifting for our application
+void applicationTask(void *param)
 {
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  CommandDetector *commandDetector = static_cast<CommandDetector *>(param);
 
-  // To send out a ping for detecting distance we need to activate the trigPin for 10microsecs
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Record the duration needed for the return signal
-  duration = pulseIn(echoPin, HIGH);
-
-  distance = duration * 0.034 * 2; // Converting from time to distance for easier use
-
-  return distance;
-}
-
-// function that activates an action tied to pin 22;
-void engageAction()
-{
-  // Activate LED
-  digitalWrite(actionPin, HIGH);
-  delay(10);
-}
-
-// function that deactivates an action tied to pin 22;
-void disengageAction()
-{
-  // Deactivate LED
-  digitalWrite(actionPin, LOW);
-  delay(10);
-}
-
-// function that checks if there is something in front of the distance senzor that is making noise, if YES it turns on an LED
-bool isSomethingThereAndNoisy(bool noise, bool distance)
-{
-  if (noise && distance)
+  const TickType_t xMaxBlockTime = pdMS_TO_TICKS(100);
+  while (true)
   {
-    return true;
-  }
-  else
-  {
-    return false;
-  }
-}
-// Loop that continually checks the status of the microphone and changes bool depending on noise
-void micLoop()
-{
-  if (digitalRead(micPinDigital) == HIGH)
-  {
-    soundActive = true;
-  }
-  else
-  {
-    soundActive = false;
-  }
-}
-
-// loop that continually measures the distance from the senzor and calls functions to activate/deactivate TBD depending on the desired distance treshold
-void distanceLoop(int distanceThresh)
-{
-  if (measureDistance() < distanceThresh)
-  {
-    distanceActive = true;
-  }
-  else
-  {
-    distanceActive = false;
-  }
-}
-
-void connectToWifi()
-{
-  // WiFi activation and connection
-  WiFi.mode(WIFI_STA);
-  WiFi.begin(SSID, SSIDpassword);
-  Serial.println("Trying to connect to WiFi...");
-
-  while (WiFi.status() != WL_CONNECTED)
-  {
-    Serial.print(".");
-    delay(200);
-    timeOutCount++;
-
-    if (timeOutCount > 20)
+    // wait for some audio samples to arrive
+    uint32_t ulNotificationValue = ulTaskNotifyTake(pdTRUE, xMaxBlockTime);
+    if (ulNotificationValue > 0)
     {
-      ESP.restart();
+      commandDetector->run();
     }
-  }
-}
-
-// Turns on the RGB led with amounts ranging from 0-255
-void controlRGB(int redAmount, int grnAmount, int bluAmount)
-{
-  analogWrite(redPin, redAmount);
-  analogWrite(grnPin, grnAmount);
-  analogWrite(bluAmount, bluAmount);
-}
-
-// test if RGB led works with changeable delay between each diode
-void testRGB(int delayAmount)
-{
-  analogWrite(redPin, 255);
-  delay(delayAmount);
-  analogWrite(redPin, 0);
-  delay(delayAmount);
-  analogWrite(grnPin, 255);
-  delay(delayAmount);
-  analogWrite(grnPin, 0);
-  delay(delayAmount);
-  analogWrite(bluPin, 255);
-  delay(delayAmount);
-  analogWrite(bluPin, 0);
-  delay(delayAmount);
-}
-// main loop that acutally starts off everything
-void workingLoop()
-{
-  if (isSomethingThereAndNoisy(soundActive, distanceActive))
-  {
-    controlRGB(255, 255, 255);
-  }
-  else
-  {
-    controlRGB(0, 0, 0);
   }
 }
 
 void setup()
 {
-  // serial monitoring
   Serial.begin(115200);
-  Serial.println("Serial begin!");
-  // pin setup (distance sensor)
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  // pin setup microphone
-  pinMode(micPinDigital, INPUT);
-  pinMode(micPinAnalogue, INPUT);
-  // pin setup(rgb led)
-  pinMode(redPin, OUTPUT);
-  pinMode(grnPin, OUTPUT);
-  pinMode(bluPin, OUTPUT);
-  // pin setup(action pin)
-  pinMode(actionPin, OUTPUT);
+  delay(1000);
+  Serial.println("Starting up");
 
-  connectToWifi();
-  testRGB(250);
+  // make sure we don't get killed for our long running tasks
+  esp_task_wdt_init(10, false);
+
+  // start up the I2S input (from either an I2S microphone or Analogue microphone via the ADC)
+#ifdef USE_I2S_MIC_INPUT
+  // Direct i2s input from INMP441 or the SPH0645
+  I2SSampler *i2s_sampler = new I2SMicSampler(i2s_mic_pins, false);
+#else
+  // Use the internal ADC
+  I2SSampler *i2s_sampler = new ADCSampler(ADC_UNIT_1, ADC_MIC_CHANNEL);
+#endif
+  // the command processor
+  CommandProcessor *command_processor = new CommandProcessor();
+
+  // create our application
+  CommandDetector *commandDetector = new CommandDetector(i2s_sampler, command_processor);
+
+  // set up the i2s sample writer task
+  TaskHandle_t applicationTaskHandle;
+  xTaskCreatePinnedToCore(applicationTask, "Command Detect", 8192, commandDetector, 1, &applicationTaskHandle, 0);
+
+  // start sampling from i2s device - use I2S_NUM_0 as that's the one that supports the internal ADC
+#ifdef USE_I2S_MIC_INPUT
+  i2s_sampler->start(I2S_NUM_0, i2sMemsConfigBothChannels, applicationTaskHandle);
+#else
+  i2s_sampler->start(I2S_NUM_0, adcI2SConfig, applicationTaskHandle);
+#endif
 }
 
-// main loop
 void loop()
 {
-  distanceLoop(distanceThreshold);
-  micLoop();
-  workingLoop();
+  vTaskDelay(pdMS_TO_TICKS(1000));
 }
